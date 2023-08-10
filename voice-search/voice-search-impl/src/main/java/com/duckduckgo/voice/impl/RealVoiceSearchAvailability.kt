05aefa1672be09c670e11329efb240d27913e92c
Cris Barreiro <cbarreiro@duckduckgo.com>
2023-08-10 11:23:58 +0200

    Remove voice search allow list

--- voice-search/voice-search-impl/src/main/java/com/duckduckgo/voice/impl/RealVoiceSearchAvailability.kt
+++ voice-search/voice-search-impl/src/main/java/com/duckduckgo/voice/impl/RealVoiceSearchAvailability.kt
@@ -31,14 +31,10 @@ class RealVoiceSearchAvailability @Inject constructor(
         private const val URL_DDG_SERP = "https://duckduckgo.com/?"
     }
 
-    private val allowList = listOf("pixel 6", "pixel 6 pro")
-
     override val isVoiceSearchSupported: Boolean
         get() = configProvider.get().run {
-            hasValidDevice(deviceModel.lowercase()) && hasValidVersion(sdkInt) && isOnDeviceSpeechRecognitionSupported && hasValidLocale(languageTag)
+            hasValidVersion(sdkInt) && isOnDeviceSpeechRecognitionSupported && hasValidLocale(languageTag)
         }
-
-    private fun hasValidDevice(model: String) = allowList.contains(model)
 
     private fun hasValidVersion(sdkInt: Int) = sdkInt >= Build.VERSION_CODES.S
 
